dad80087dcd8cd5d4819cf1334cb8f86bfa58170
ErvinAllen <ervin.allen.ea@gmail.com>
2024-11-13 19:05:02 +0330

    Finish Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,62 @@
 import { Link } from "react-router-dom"
-import { NavbarIcon } from "../components/navbarIcon"
+import { useState } from "react"
+import { motion } from "framer-motion"
 
 function Home() {
-  
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  const navBarIcon = (
+    <label className="btn btn-circle swap swap-rotate md:hidden backdrop-blur-xl p-0">
+        {/* this hidden checkbox controls the state */}
+        <input type="checkbox" onClick={() => { setIsSideBarOpen(!isSideBarOpen) }} />
+      
+        {/* hamburger icon */}
+        <svg
+          className="swap-off fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 512 512">
+          <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+        </svg>
+      
+        {/* close icon */}
+        <svg
+          className="swap-on fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 512 512">
+          <polygon
+            points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+        </svg>
+      </label>
+  )
   return (
     <div className="w-screen h-screen">
+      <motion.div 
+      initial={{
+        x: -192
+      }}
+      animate={{
+        x: isSideBarOpen ? 0 : -192
+      }}
+      transition={{
+        type: 'tween',
+        duration: 0.15
+      }}
+      className={`bg-neutral-900 bg-opacity-60 backdrop-blur-md h-screen fixed z-30 flex flex-col p-8 pt-16 gap-y-8 md:hidden w-48`}>
+        <a href="/" className="hover:scale-110 hover:text-red-400 transition-all ease-in-out">About Us</a>
+        <a href="/" className="hover:scale-110 hover:text-red-400 transition-all ease-in-out">Contact Us</a>
+        <a href="/" className="hover:scale-110 hover:text-red-400 transition-all ease-in-out">Privacy Policy</a>
+        <a href="/" className="hover:scale-110 hover:text-red-400 transition-all ease-in-out">Terms of Service</a>
+        <a href="/" className="hover:scale-110 hover:text-red-400 transition-all ease-in-out">FAQ</a>
+      </motion.div>
         <div className="w-full h-full bg-[url('./assets/backGround.jpg')] bg-cover bg-center flex justify-center items-center">
-
+        
         <nav className="z-20 w-full h-auto fixed bg-gradient-to-b from-stone-950 bg-opacity-70 top-0">
           <ul className="flex md:flex-row h-24  text-nowrap items-center p-8 justify-between text-white text-lg">
             <li className="text-4xl font-serif"><a href="/">Movie<span className="text-red-500">Master</span></a></li>
-            <NavbarIcon />
+            {navBarIcon}
             <ul className="md:flex hidden md:flex-row gap-4 px-4">
               <a href="/" className="hover:scale-110 hover:text-red-400 transition-all ease-in-out">About Us</a>
               <a href="/" className="hover:scale-110 hover:text-red-400 transition-all ease-in-out">Contact Us</a>
@@ -21,11 +67,10 @@ function Home() {
           </ul>
         </nav>
 
-          <div className="border border-stone-700 p-8 w-full m-8 mx-[20%] z-10 bg-zinc-950 bg-opacity-25 backdrop-blur-md rounded-3xl mt-28">
-            <h1 className="text-gray-300 text-nowrap text-center text-3xl font-bold">Welcome to MovieMaster</h1>
-            <p className="text-white/70 text-center py-6">Please log in to explore movies, watch trailers, and discover your next favorite film. Dive into the world of cinema with MovieMaster!</p>
+          <div className="border border-stone-700 p-8 mx-[20%] z-10 bg-zinc-950 bg-opacity-50 backdrop-blur-md rounded-3xl mt-24">
+            <h1 className="text-red-200 text-nowrap text-xl text-center sm:text-3xl font-bold">Welcome to MovieMaster</h1>
+            <p className="text-white/70 text-center text-sm py-6">Please log in to explore movies, watch trailers, and discover your next favorite film. Dive into the world of cinema with MovieMaster!</p>
             <div className="border border-black/40 w-full" />
-
             <form className="pt-8 flex flex-col gap-1" action="">
             <label className="input input-bordered flex items-center gap-2">
               <svg
@@ -54,8 +99,8 @@ function Home() {
               </label>
 
               <div className="flex flex-col gap-1">
-                <button className="btn btn-block">Log In</button>
-                <button className="btn btn-block">Sign Up</button>
+                <button className="btn btn-block bg-red-500 text-white hover:bg-red-900">Log In</button>
+                <button className="btn btn-block bg-red-500 text-white hover:bg-red-900">Sign Up</button>
                 <Link to="/discover"><button className="btn btn-block border-none bg-blue-600 text-white hover:bg-blue-900">Log In as a Guest</button></Link>
               </div>
             </form>
